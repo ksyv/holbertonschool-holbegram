33d9aeaae6da7e90e7380980041f305d4ee4eddb
ksyv <ksyviais@gmail.com>
2025-12-17 15:02:40 +0100

    Task 1: create reusable TextFieldInput widget with custom styling

--- holbegram/lib/widgets/text_field.dart
+++ holbegram/lib/widgets/text_field.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
 
 class TextFieldInput extends StatelessWidget {
-  const TextFieldInput({super.key});
+    final TextEditingController controller;
+    final bool isPassword;
+    final String hintText;
+    final Widget? suffixIcon;
+    final TextInputType keyboardType;
+
+  const TextFieldInput({
+    super.key,
+    required this.controller,
+    required this.isPassword,
+    required this.hintText,
+    this.suffixIcon,
+    required tis.keyboardType,
+    });
 
   @override
   Widget build(BuildContext context) {
-    return const TextField();
+    return TextField(
+        controller: controller,
+        cursorColor: const Color.fromARGB(218, 226, 37, 24),
+        keybordType: keyboardType,
+        obscureText: isPassword,
+        textInputAction: TextInputAction.next,
+
+        decoration: InputDecoration(
+            hintText: hintText,
+            filled: true,
+            contentPadding: const EdgeInsets.all(8),
+            suffixIcon: suffixIcon,
+            border: const OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.transparent, style: BorderStyle.none),
+            ),
+            focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.transparent, style: BorderStyle.none),
+            ),
+            enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.transparent, style: BorderStyle.none),
+            ),
+        ),
+    );
   }
 }
