11a37aa29a595d5d12c58f27deb2431e630fd91c
ksyv <ksyviais@gmail.com>
2026-01-21 15:43:28 +0100

    feat: implement search screen with staggered grid view

--- holbegram/lib/screens/pages/search.dart
+++ holbegram/lib/screens/pages/search.dart
@@ -1,12 +1,73 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart'; 
 
-class Search extends StatelessWidget {
+class Search extends StatefulWidget {
   const Search({super.key});
 
   @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Search")),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+            color: Colors.grey[200],
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: TextField(
+              controller: _searchController,
+              decoration: const InputDecoration(
+                hintText: 'Search',
+                border: InputBorder.none,
+                icon: Icon(Icons.search, color: Colors.grey),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return const Center(child: Text("No posts to show"));
+          }
+
+          return MasonryGridView.count(
+            crossAxisCount: 3,
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              final post = snapshot.data!.docs[index].data();
+              return Image.network(
+                post['postUrl'],
+                fit: BoxFit.cover,
+              );
+            },
+            mainAxisSpacing: 8.0,
+            crossAxisSpacing: 8.0,
+          );
+        },
+      ),
     );
   }
 }
